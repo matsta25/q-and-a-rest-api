831d18216673068091c8b0e7b81a4d185195f906
Mateusz Stańczak <mateusz.stanczak@pgf.com.pl>
2021-03-13 19:23:53 +0100

    feat(app): INIT

---
+++ src/main/java/com/matsta25/qandarestapi/QAndARestApiApplication.java
@@ -0,0 +1,13 @@
+package com.matsta25.qandarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAndARestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QAndARestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matsta25/qandarestapi/QAndARestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsta25.qandarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QAndARestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
